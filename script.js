c4984f557f22547e88758b04b28a633cbe4e7d8d
Oscarrscso <118562321+Oscarrscso@users.noreply.github.com>
2023-01-22 02:16:30 +0000

    very basic functionality add

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 let operator = ''
-let curr = ''
-let prev = ''
-let prevD = document.getElementById("prevD")
-let currD = document.getElementById("currD")
+
 let btns = document.querySelectorAll("button")
 
+let currVal = ''
+let prevVal = ''
+
+let prevDisp = document.getElementById("prevD")
+let currDisp = document.getElementById("currD")
+
+//################################################################################################
 
 btns.forEach(el =>{
     el.addEventListener("click", e =>{
-
         let classCheck = e.target.className
         let id = e.target.id
-
-//check if operator btn was clicked, store the operator in a variable.
-            if(classCheck == "oper"){
+            if(classCheck == "oper" || classCheck == "AC" || classCheck == "DEL"){
                switch(id){
                 case 'plus': operator = id
                     break
@@ -23,7 +24,29 @@ btns.forEach(el =>{
                     break
                 case 'divide': operator = id
                     break
-               } 
-            }
+                case 'AC': ac()
+                    break
+                case 'DEL': return
+                    default: return
+            } 
+            
+        } else {
+        if(currVal.length < 10){
+                console.log("aaa");
+            
+            currVal += id
+                currDisp.innerHTML = currVal
+                } else {
+                    return
+                        } 
+        }
     })
 })
+
+const ac = () =>{
+    currVal = '', 
+    prevVal = '', 
+    currDisp.innerHTML = '',
+    prevDisp.innerHTML = '', 
+    operator = ''
+}
